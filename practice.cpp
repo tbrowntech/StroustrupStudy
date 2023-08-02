3e757ed6b750febddc240441683e073837d9b071
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-08-02 19:28:50 +0000

    changes to practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main() {
-    std::cout << "Hello, World!\n";
-    return 0;
+    // use an array to sort the values in the array and then print them in an ordered list
+    return 0;    
 }
