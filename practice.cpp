97f18c8556e3a4b36292b3351dfcd696fbf4d920
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-08-10 15:17:49 +0000

    adding practice file

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    int counter = 5;
+
+    while(counter--) {
+        cout << "I am an awesome program." << endl;
+    }
+
+    return 0;
+}
