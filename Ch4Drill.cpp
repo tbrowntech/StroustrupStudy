2a7f7e32b52a5fb63428e4aadc634a863eb7edb0
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-08-10 21:35:49 +0000

    updates to Ch4Drill

--- Ch4Drill.cpp
+++ Ch4Drill.cpp
@@ -8,7 +8,42 @@
 using namespace std;
 
 int main() {
-    double i = 0;
+    vector<double> nums;
+    double num;
+    int i = 0;
+    while (i < 2) {
+        cin >> num;
+        nums.push_back(num);
+        i++;
+        if (i != 1) {
+
+            auto smaller = min_element(begin(nums), end(nums));
+            auto larger = max_element(begin(nums), end(nums));
+            if (end(nums) != smaller && larger != smaller) {
+                cout << "The smaller value is: " << *smaller << endl;
+            }
+
+            if (end(nums) != larger && larger != smaller) {
+                cout << "The larger value is: " << *larger << endl;
+            }
+        
+            if (smaller == larger) {
+                cout << "the numbers are equal" << endl;
+            }
+
+            if (abs(smaller - larger) <= 0.01 && larger != smaller) {
+                cout << "the numbers are almost equal" << endl;
+            }
+        }
+    }
+    
+    /*for (int i = 0; i < nums.size(); ++i) {
+        cout << nums[i] << endl;
+    }*/
+
+    
+  
+    /*int i = 0;
     double number1, number2;
     cin >> number1;
     cin >> number2;
@@ -31,5 +66,5 @@ int main() {
             cout << "the numbers are equal" << endl;
         } 
         i++;
-    }
+    }*/
 }
