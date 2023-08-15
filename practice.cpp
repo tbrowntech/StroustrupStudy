9aa35ba3a94e65527248df98411de2c03e2796b4
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-08-15 17:19:50 +0000

    edited practice

--- practice.cpp
+++ practice.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include "std_lib_facilities.h"
 
-using std::cout;
-using std::endl;
+using namespace std;
 
 int main() {
-    int x0 = arena(7);
-    int x1 = area(7);
-    int x2 = area("seven", 2);
+    double sum = 0;
+    double high_temp = {};
+    double low_temp = {};
+    int no_of_temps = 0;
 
-    return 0;
+    vector<double> temps;
+
+    for (double temp; cin >> temp;) {
+        ++no_of_temps;
+        sum += temp;
+        if (temp > high_temp) {
+            high_temp = temp;
+        }
+        if (temp < low_temp) {
+            low_temp = temp;
+        }
+    }    
+    cout << "High temperature: " << high_temp << endl;
+    cout << "Low temperature: " << low_temp << endl;
+    if (no_of_temps != 0) {
+        cout << "Average temperature: " << sum/no_of_temps << endl;
+    }
 }
