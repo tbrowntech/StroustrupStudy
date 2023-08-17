b66414d5f4d6a5e7cfbe087c592fd1c421dc2a28
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-08-17 18:48:47 +0000

    changes to exercise

--- Ch5Exercise.cpp
+++ Ch5Exercise.cpp
@@ -1,20 +1,78 @@
 #include "std_lib_facilities.h"
 
+double temp = 0;
+char unit = {};
+
 double ctok(double c) // converts Celsius to Kelvin
     {
-        if (c > -273.15) {
+        if (c > -273.15) 
+        {
             double k = c + 273.15;
             return k;
-        } else {
+        } 
+        else 
+        {
             cout << "error: value below absolute zero entered.\n";
-            return c;
+            return 1;
         } 
     }
 
-int main() {
-    double c = 0;
-    cin >> c;
+double ktoc(double k)  // converts Kelvin to Celcius
+{
+    if (k >= 0)
+    {
+        double c = k - 273.15;
+        return c;
+    }
+    else
+    {
+        cout << "error: k cannot be less than 0 (absolute 0)";
+        return 1;
+    }
+}
 
-    double calc = ctok(c);
-    cout << calc << endl;
+char getUnit()
+{
+    cout << "This program converts Kelvin (k) to Celcius (c), or Celcius to\n" 
+            "Kelvin. From which unit would you like to convert? (k or c) ";
+    cin >> unit;
+    if (unit == 'k' || unit == 'K')
+    {
+        return 'k';
+    }
+    else if (unit == 'c' || unit == 'C')
+    {
+        return 'c';
+    }
+    else
+    {
+        cout << "invalid entry, please enter k or c\n";
+        getUnit();
+    }
+    return 0;
+}
+
+void getTemp() 
+{
+    cout << "What is the temperature? ";
+    cin >> temp;
+}
+
+double calcTemp(char unit)
+{
+    if (unit == 'k')
+    {
+        getTemp();
+        ktoc(temp);
+    }
+    else if (unit == 'c')
+    {
+        getTemp();
+        ctok(temp);
+    }
+}
+
+int main() {
+    getUnit();
+    pickFormula(unit);
 }
