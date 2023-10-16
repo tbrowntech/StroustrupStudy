2dd4b2b9dcb0961703e447d17e11647527388130
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-10-16 21:35:37 +0000

    adding function to make phone call

--- practice.cpp
+++ practice.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 class Smartphone {
 public:
-    virtual void TakeASelfie()=0;
+    virtual void TakeASelfie() = 0;
+    virtual void MakeACall() = 0;
 };
 
 class Android:public Smartphone {
@@ -11,12 +12,18 @@ public:
     void TakeASelfie() {
         cout << "Android selfie\n";
     }
+    void MakeACall() {
+        cout << "Android calling\n";
+    }
 };
 
 class IPhone:public Smartphone {
 public:
     void TakeASelfie() {
         cout << "IPhone selfie\n";
+    }
+    void MakeACall() {
+        cout << "IPhone calling\n";
     }
 };
 
@@ -26,5 +33,7 @@ int main()
     Smartphone* s2 = new IPhone();
     s1->TakeASelfie();
     s2->TakeASelfie();
+    s1->MakeACall();
+    s2->MakeACall();
     return 0;
 }
