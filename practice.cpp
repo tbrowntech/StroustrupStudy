d22a7f989c4e3d515742f5f0e42bf8204133e58f
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-10-16 21:26:09 +0000

    adding new example to practice

--- practice.cpp
+++ practice.cpp
@@ -1,45 +1,13 @@
 #include <iostream>
-#include "std_lib_facilities.h"
-
 using namespace std;
 
-class User {
+class Smartphone {
 public:
-    string FirstName;
-    string LastName;
-    int Age;
-    string Email;
-
-    // default constructor
-    User() {
-        FirstName = "nn";
-        LastName = "nln";
-        Age = 0;
-        Email = "not set";
-    }
-    // params constructor
-    User(string fName, string lName, int age) {
-        FirstName = fName;
-        LastName = lName;
-        Age = age;
-        Email = fName + "." + lName + "@mail.com";
-    }
+    virtual void TakeASelfie()=0;
 };
 
-void getUserInfo(User u) {
-    cout << "First Name: " << u.FirstName << endl;
-    cout << "Last Name: " << u.LastName << endl;
-    cout << "Age: " << u.Age << endl;
-    cout << "Email: " << u.Email << endl;
-}
-
-int main() {
-    User user1("Taylor", "Brown", 32);
-    User user2("Kallie", "Brown", 33);
-
-    getUserInfo(user1);
-    cout << endl;
-    getUserInfo(user2);
-
-    cin.get();
+int main()
+{
+    cout << "hello world\n";
+    return 0;
 }
