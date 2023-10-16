c9cdc23dfd48b390cdb0c3d4ab52b5e22198f379
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-10-16 21:32:36 +0000

    adding another class for practice

--- practice.cpp
+++ practice.cpp
@@ -6,8 +6,25 @@ public:
     virtual void TakeASelfie()=0;
 };
 
+class Android:public Smartphone {
+public:
+    void TakeASelfie() {
+        cout << "Android selfie\n";
+    }
+};
+
+class IPhone:public Smartphone {
+public:
+    void TakeASelfie() {
+        cout << "IPhone selfie\n";
+    }
+};
+
 int main()
 {
-    cout << "hello world\n";
+    Smartphone* s1 = new Android();
+    Smartphone* s2 = new IPhone();
+    s1->TakeASelfie();
+    s2->TakeASelfie();
     return 0;
 }
