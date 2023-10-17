b8cb1b19ba182a131d30e7d6e3ef23c62969f490
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2023-10-17 16:27:00 +0000

    resetting practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,39 +1,7 @@
-#include <iostream>
-using namespace std;
-
-class Smartphone {
-public:
-    virtual void TakeASelfie() = 0;
-    virtual void MakeACall() = 0;
-};
-
-class Android:public Smartphone {
-public:
-    void TakeASelfie() {
-        cout << "Android selfie\n";
-    }
-    void MakeACall() {
-        cout << "Android calling\n";
-    }
-};
-
-class IPhone:public Smartphone {
-public:
-    void TakeASelfie() {
-        cout << "IPhone selfie\n";
-    }
-    void MakeACall() {
-        cout << "IPhone calling\n";
-    }
-};
+#include "std_lib_facilities.h"
 
 int main()
 {
-    Smartphone* s1 = new Android();
-    Smartphone* s2 = new IPhone();
-    s1->TakeASelfie();
-    s2->TakeASelfie();
-    s1->MakeACall();
-    s2->MakeACall();
+    cout << "hello world";
     return 0;
 }
